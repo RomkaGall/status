6942bd191eb2b6e43f91f3399e87727e8afbe622
RomkaGall <romangalanuk@gmail.com>
2020-11-05 17:15:26 +0200

    banner

--- js/banner.js
+++ js/banner.js
@@ -1,195 +1,213 @@
-let progressBar1; 
-let progressBar2; 
-let progressBar3; 
-let progressBar4; 
-let video_duration1;
-let video_duration2; 
-let video_duration3; 
-let video_duration4;
-let player1;
-let player2;
-let player3;
-let player4;
-let playerId;
-let videoId;
-let playerVarsList = {
-    'controls': 0,
-    'disablekb' : 1,
-    'enablejsapi': 1,
-    'fs': 0,
-    'iv_load_policy': 3,
-    'modestbranding': 1,
-    'rel': 0,
-    'showinfo': 0,
-    'cc_load_policy': 0
-}
+// let progressBar1; 
+// let progressBar2; 
+// let progressBar3; 
+// let progressBar4; 
+// let video_duration1;
+// let video_duration2; 
+// let video_duration3; 
+// let video_duration4;
+// let player1;
+// let player2;
+// let player3;
+// let player4;
+// let playerId;
+// let videoId;
+// let playerVarsList = {
+//     'controls': 0,
+//     'disablekb' : 1,
+//     'enablejsapi': 1,
+//     'fs': 0,
+//     'iv_load_policy': 3,
+//     'modestbranding': 1,
+//     'rel': 0,
+//     'showinfo': 0,
+//     'cc_load_policy': 0
+// }
 
-function onYouTubeIframeAPIReady(state) {
-    player1 = new YT.Player('player1', {
-    videoId: '1La4QzGeaaQ',
-    playerVars : playerVarsList,
-    events: {
-        'onReady': onPlayerReady1,
-        'onStateChange': onPlayerStateChange1
-    }
-    });
-    player2 = new YT.Player('player2', {
-    videoId: 'LfmdPsXeOTM',
-    playerVars : playerVarsList,
-    events: {
-        'onReady': onPlayerReady2,
-        'onStateChange': onPlayerStateChange2
-    }
-    });
-    player3 = new YT.Player('player3', {
-    videoId: 'TvWcU3aztmo',
-    playerVars : playerVarsList,
-    events: {
-        'onReady': onPlayerReady3,
-        'onStateChange': onPlayerStateChange3
-    }
-    });
-    player4 = new YT.Player('player4', {
-    videoId: '7LJIcrJKDI0',
-    playerVars : playerVarsList,
-    events: {
-        'onReady': onPlayerReady4,
-        'onStateChange': onPlayerStateChange4
-    }
-    });
+// function onYouTubeIframeAPIReady(state) {
+//     player1 = new YT.Player('player1', {
+//     videoId: '1La4QzGeaaQ',
+//     playerVars : playerVarsList,
+//     events: {
+//         'onReady': onPlayerReady1,
+//         'onStateChange': onPlayerStateChange1
+//     }
+//     });
+//     player2 = new YT.Player('player2', {
+//     videoId: 'LfmdPsXeOTM',
+//     playerVars : playerVarsList,
+//     events: {
+//         'onReady': onPlayerReady2,
+//         'onStateChange': onPlayerStateChange2
+//     }
+//     });
+//     player3 = new YT.Player('player3', {
+//     videoId: 'TvWcU3aztmo',
+//     playerVars : playerVarsList,
+//     events: {
+//         'onReady': onPlayerReady3,
+//         'onStateChange': onPlayerStateChange3
+//     }
+//     });
+//     player4 = new YT.Player('player4', {
+//     videoId: '7LJIcrJKDI0',
+//     playerVars : playerVarsList,
+//     events: {
+//         'onReady': onPlayerReady4,
+//         'onStateChange': onPlayerStateChange4
+//     }
+//     });
     
-}
-function onPlayerStateChange1(event) {
-    video_duration1 = player1.getDuration()
-}
-function onPlayerStateChange2(event) {
-    video_duration2 = player2.getDuration()
-}
-function onPlayerStateChange3(event) {
-    video_duration3 = player3.getDuration()
-}
-function onPlayerStateChange4(event) {
-    video_duration4 = player4.getDuration()
-}
-function pauseVideo1() {
-    player1.pauseVideo();
-    clearInterval(progressBar1)
-}
-function pauseVideo2() {
-    player2.pauseVideo();
-    clearInterval(progressBar2)
-}
-function pauseVideo3() {
-    player3.pauseVideo();
-    clearInterval(progressBar3)
-}
-function pauseVideo4() {
-    player4.pauseVideo();
-    clearInterval(progressBar4)
-}
-function playVideo1() {
-    player1.playVideo();
-    player1.mute();
+// }
+// function onPlayerStateChange1(event) {
+//     video_duration1 = player1.getDuration()
+// }
+// function onPlayerStateChange2(event) {
+//     video_duration2 = player2.getDuration()
+// }
+// function onPlayerStateChange3(event) {
+//     video_duration3 = player3.getDuration()
+// }
+// function onPlayerStateChange4(event) {
+//     video_duration4 = player4.getDuration()
+// }
+// function pauseVideo1() {
+//     player1.pauseVideo();
+//     clearInterval(progressBar1)
+// }
+// function pauseVideo2() {
+//     player2.pauseVideo();
+//     clearInterval(progressBar2)
+// }
+// function pauseVideo3() {
+//     player3.pauseVideo();
+//     clearInterval(progressBar3)
+// }
+// function pauseVideo4() {
+//     player4.pauseVideo();
+//     clearInterval(progressBar4)
+// }
+// function playVideo1() {
+//     player1.playVideo();
+//     player1.mute();
     
-    progressBar1 = setInterval(() => {
-        $('.video_progress1 .bar').css('width', `${(player1.getCurrentTime() / video_duration1) * 100}%`)
-    }, 100);
-}
-function playVideo2() {
-    player2.playVideo();
-    player2.mute();
+//     progressBar1 = setInterval(() => {
+//         $('.video_progress1 .bar').css('width', `${(player1.getCurrentTime() / video_duration1) * 100}%`)
+//     }, 100);
+// }
+// function playVideo2() {
+//     player2.playVideo();
+//     player2.mute();
     
-    progressBar2 = setInterval(() => {
-        $('.video_progress2 .bar').css('width', `${(player2.getCurrentTime() / video_duration2) * 100}%`)
-    }, 100);
-}
-function playVideo3() {
-    player3.playVideo();
-    player3.mute();
+//     progressBar2 = setInterval(() => {
+//         $('.video_progress2 .bar').css('width', `${(player2.getCurrentTime() / video_duration2) * 100}%`)
+//     }, 100);
+// }
+// function playVideo3() {
+//     player3.playVideo();
+//     player3.mute();
     
-    progressBar3 = setInterval(() => {
-        $('.video_progress3 .bar').css('width', `${(player3.getCurrentTime() / video_duration3) * 100}%`)
-    }, 100);
-}
-function playVideo4() {
-    player4.playVideo();
-    player4.mute();
+//     progressBar3 = setInterval(() => {
+//         $('.video_progress3 .bar').css('width', `${(player3.getCurrentTime() / video_duration3) * 100}%`)
+//     }, 100);
+// }
+// function playVideo4() {
+//     player4.playVideo();
+//     player4.mute();
     
-    progressBar4 = setInterval(() => {
-        $('.video_progress4 .bar').css('width', `${(player4.getCurrentTime() / video_duration4) * 100}%`)
-    }, 100);
-}
+//     progressBar4 = setInterval(() => {
+//         $('.video_progress4 .bar').css('width', `${(player4.getCurrentTime() / video_duration4) * 100}%`)
+//     }, 100);
+// }
 
-function onPlayerReady1(event) {     
-    // player1.playVideo()
-}
-function onPlayerReady2(event) {     
-    // player2.playVideo()
-}
-function onPlayerReady3(event) {     
-    // player3.playVideo()
-}
-function onPlayerReady4(event) {     
-    // player4.playVideo()
-}
-const videoStart = () => {
-    var tag = document.createElement('script');
+// function onPlayerReady1(event) {     
+//     // player1.playVideo()
+// }
+// function onPlayerReady2(event) {     
+//     // player2.playVideo()
+// }
+// function onPlayerReady3(event) {     
+//     // player3.playVideo()
+// }
+// function onPlayerReady4(event) {     
+//     // player4.playVideo()
+// }
+// const videoStart = () => {
+//     var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
+//     tag.src = "https://www.youtube.com/iframe_api";
+//     var firstScriptTag = document.getElementsByTagName('script')[0];
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// }
 
 
-$(document).on('click', '.banner__control', function () {
-    if ($(this).hasClass('start')) {
+// $(document).on('click', '.banner__control', function () {
+//     if ($(this).hasClass('start')) {
        
-        $(this).removeClass('start')
-        videoStart() 
+//         $(this).removeClass('start')
+//         videoStart() 
         
           
-        // $(this).toggleClass('pause')
-        $(this).parents('.swiper-slide').find('.player_overlay').addClass('hidden')
-    } else {
+//         // $(this).toggleClass('pause')
+//         $(this).parents('.swiper-slide').find('.player_overlay').addClass('hidden')
+//     } else {
 
-        if($(this).hasClass('pause')) {    
-            switch ($(this).parents('.swiper-slide').find('.player_container').attr('id')) {
-                case 'player1':
-                    pauseVideo1()
-                    break;
-                case 'player2':
-                    pauseVideo2()
-                    break;
-                case 'player3':
-                    pauseVideo3() 
-                    break;
-                case 'player4':
-                    pauseVideo4() 
-                    break;
+//         if($(this).hasClass('pause')) {    
+//             switch ($(this).parents('.swiper-slide').find('.player_container').attr('id')) {
+//                 case 'player1':
+//                     pauseVideo1()
+//                     break;
+//                 case 'player2':
+//                     pauseVideo2()
+//                     break;
+//                 case 'player3':
+//                     pauseVideo3() 
+//                     break;
+//                 case 'player4':
+//                     pauseVideo4() 
+//                     break;
             
-                default:
-                    break;
-            }   
-        } else {
-            switch ($(this).parents('.swiper-slide').find('.player_container').attr('id')) {
-                case 'player1':
-                    playVideo1() 
-                    break;
-                case 'player2':
-                    playVideo2() 
-                    break;
-                case 'player3':
-                    playVideo3() 
-                    break;
-                case 'player4':
-                    playVideo4() 
-                    break;
+//                 default:
+//                     break;
+//             }   
+//         } else {
+//             switch ($(this).parents('.swiper-slide').find('.player_container').attr('id')) {
+//                 case 'player1':
+//                     playVideo1() 
+//                     break;
+//                 case 'player2':
+//                     playVideo2() 
+//                     break;
+//                 case 'player3':
+//                     playVideo3() 
+//                     break;
+//                 case 'player4':
+//                     playVideo4() 
+//                     break;
             
-                default:
-                    break;
-            }
-        }
-        $(this).toggleClass('pause')
-    }
+//                 default:
+//                     break;
+//             }
+//         }
+//         $(this).toggleClass('pause')
+//     }
     
-})
+// })
+
+jQuery(function(){
+
+    $('.youtube_player').each(function () {
+        const id = $(this).attr('id')
+        
+        jQuery(`#${id}`).YTPlayer({
+            onReady: function( player ) {
+                $(player).parents('.banner__content').find('.player_overlay').fadeOut(400)
+            }           
+        });
+    })
+    
+  });
+
+  $(document).on('click', '.banner__control', function () {
+    $(this).toggleClass('pause')
+  })
--- js/main.js
+++ js/main.js
@@ -15,24 +15,68 @@ $(document).ready(function () {
         navigation: {
             nextEl: '.swiper-button-next'
         },
+        on : {
+            slideChange: function(i) {
+                const id = swiper.$el.find('.swiper-slide').eq(swiper.activeIndex).find('.youtube_player').attr('id')
+                swiper.$el.find('.banner__control').removeClass('pause')
+                jQuery(`#${id}`).YTPPause()
+            }
+        }
     });
-    var swiperMap = new Swiper('.map__slider', {
+    var swiper2 = new Swiper('.map__slider', {
         pagination: {
-          el: '.map__slider .swiper-pagination',
+          el: '.swiper-pagination2',
           type: 'progressbar'
         },
         navigation: {
             nextEl: '.swiper-button-next'
         },
+        on : {
+            slideChange: function(i) {
+                swiper2.$el.find('.swiper-pagination-bullet').eq(swiper2.realIndex).addClass('swiper-pagination-bullet-active')
+                $('.map__slider .swiper-pagination-bullet').eq(swiper2.realIndex).siblings().removeClass('swiper-pagination-bullet-active')
+            }
+        }
     });
+
+        // add custom bullets
+    $(function () {
+        let sliderCount;
+
+        $('.custom_bullets').each(function () {
+            sliderCount = $(this).find('.swiper-slide').length
+
+            for (let i = 0; i < sliderCount; i++) {
+                $(this).find('.swiper_bullets').append(`<span class="swiper-pagination-bullet"></span>`)
+            }
+
+            $(this).find('.swiper-pagination-bullet').eq(0).addClass('swiper-pagination-bullet-active')
+
+            $(document).on('click',  '.custom_bullets .swiper-pagination-bullet', function() {
+                let slider = $(this).parents('.custom_bullets').data('swiper')
+                $(this).addClass('swiper-pagination-bullet-active').siblings().removeClass('swiper-pagination-bullet-active')
+                const index = $(this).index()
+
+                switch (slider) {
+                    case 'swiper2':
+                        swiper2.slideTo(index)
+                        break;
+                
+                    default:
+                        break;
+                }
+             });
+        })
+    })    
     // sliders end
 
     // tabs
       $(document).on('click', '.tab__nav_item', function () {
         $(this).addClass('active').siblings().removeClass('active')  
-        $(`*[data-tab]`).not('.tab__nav_item').hide();
-        $(`*[data-tab='${$(this).data('tab')}']`).show();
+        $(`*[data-tab]`).not('.tab__nav_item').removeClass('active');
+        $(`*[data-tab='${$(this).data('tab')}']`).addClass('active');
       })
     // tabs end
+    
 });
 
