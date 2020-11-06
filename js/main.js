62e307dc16003d63c473258a1598528682a7b5ee
RomkaGall <romangalanuk@gmail.com>
2020-11-06 04:46:35 +0200

    main

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
           type: 'progressbar'
         },
         navigation: {
-            nextEl: '.swiper-button-next'
+            nextEl: '.map__slider .swiper-button-next'
         },
         on : {
             slideChange: function(i) {
@@ -37,6 +37,87 @@ $(document).ready(function () {
                 $('.map__slider .swiper-pagination-bullet').eq(swiper2.realIndex).siblings().removeClass('swiper-pagination-bullet-active')
             }
         }
+    });
+
+    var swiper3 = new Swiper('.apartments__slider', {
+        pagination: {
+          el: '.swiper-pagination3',
+          type: 'progressbar'
+        },
+        navigation: {
+            nextEl: '.apartments__slider .swiper-button-next'
+        },
+        on : {
+            init: function (i) {
+                const title = i.slides[i.activeIndex].dataset['title']
+                const profit = i.slides[i.activeIndex].dataset['profit']
+                const year = i.slides[i.activeIndex].dataset['year']
+                const month = i.slides[i.activeIndex].dataset['month']
+                const area = i.slides[i.activeIndex].dataset['area']
+                const sum = i.slides[i.activeIndex].dataset['sum']
+                const plan = i.slides[i.activeIndex].dataset['plan']
+                // const image = 
+
+                i.$el.find('.slider_title').text(title)
+                i.$el.find('.slider_info_text').text(`Доход в год: ${profit} ₽`)
+
+                $('.apartments__block_title').text(title)
+                $('.apartments__values_item--year .value').text(`${year} ₽`)
+                $('.apartments__values_item--month .value').text(`${month} ₽`)
+                $('.apartments__values_item--area .value').text(`${area} ₽`)
+                $('.apartments__values_item--sum .value').text(`${sum} ₽`)
+                $('.popup--plan .popup__image').attr('src', `img/content/${plan}`)
+                
+            },
+            slideChange: function(i) {
+                swiper3.$el.find('.swiper-pagination-bullet').eq(swiper3.realIndex).addClass('swiper-pagination-bullet-active')
+                $('.apartments__slider .swiper-pagination-bullet').eq(swiper3.realIndex).siblings().removeClass('swiper-pagination-bullet-active')
+
+                const title = i.slides[i.activeIndex].dataset['title']
+                const profit = i.slides[i.activeIndex].dataset['profit']
+                const year = i.slides[i.activeIndex].dataset['year']
+                const month = i.slides[i.activeIndex].dataset['month']
+                const area = i.slides[i.activeIndex].dataset['area']
+                const sum = i.slides[i.activeIndex].dataset['sum']
+                const plan = i.slides[i.activeIndex].dataset['plan']
+
+                i.$el.find('.slider_title').text(title)
+                i.$el.find('.slider_info_text').text(`Доход в год: ${profit} ₽`)
+
+                $('.apartments__block_title').text(title)
+                $('.apartments__values_item--year .value').text(`${year} ₽`)
+                $('.apartments__values_item--month .value').text(`${month} ₽`)
+                $('.apartments__values_item--area .value').text(`${area} ₽`)
+                $('.apartments__values_item--sum .value').text(`${sum} ₽`)
+                $('.popup--plan .popup__image').attr('src', `img/content/${plan}`)
+            }
+        }
+    });
+
+    var swiper4 = new Swiper('.infrastructure__slider', {
+        pagination: {
+          el: '.swiper-pagination4',
+          type: 'progressbar'
+        },
+        navigation: {
+            nextEl: '.infrastructure__slider .swiper-button-next'
+        },
+        on : {
+            slideChange: function(i) {
+                swiper4.$el.find('.swiper-pagination-bullet').eq(swiper2.realIndex).addClass('swiper-pagination-bullet-active')
+                $('.infrastructure__slider .swiper-pagination-bullet').eq(swiper2.realIndex).siblings().removeClass('swiper-pagination-bullet-active')
+            }
+        }
+    });
+
+    var swiper5 = new Swiper('.team__slider', {
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 50,
+        slidesPerView: 4,
+        centerInsufficientSlides: true,
+        loop: true,
+        autoHeight: true
     });
 
         // add custom bullets
@@ -78,5 +159,23 @@ $(document).ready(function () {
       })
     // tabs end
     
+    $(document).on('click', '.apartments__show_plan', function () {
+        $(".popup--plan").addClass("show");
+    })
+
+    $(document).on("click", ".close", function () {
+        $(".popup").removeClass("show");
+    });
+
+    $(document).on("click touchstart", function (e) {
+        if (
+            !$(e.target).closest('*[data-toggle="modal"]').length &&
+            !$(e.target).closest(".popup__content").length 
+        ) {
+            $(e.target).removeClass("show");
+        }
+
+        e.stopPropagation();
+    });
 });
 
