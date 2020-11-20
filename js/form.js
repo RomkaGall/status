9a8f86fea65680519dd9dff4540b03d60aa1370e
RomkaGall <romangalanuk@gmail.com>
2020-11-20 18:20:37 +0200

    scroll

--- js/form.js
+++ js/form.js
@@ -65,26 +65,52 @@ $(document).ready(function () {
         $(this).parent().next().addClass('hidden')
     })
 
-    function SmoothVerticalScrolling(e, time, where) {
-        var eTop = e.getBoundingClientRect().top;
-        var eAmt = eTop / 100;
-        var curTime = 0;
-        while (curTime <= time) {
-            window.setTimeout(SVS_B, curTime, eAmt, where);
-            curTime += time / 100;
-        }
-    }
+    // function SmoothVerticalScrolling(e, time, where) {
+    //     var eTop = e.getBoundingClientRect().top;
+    //     var eAmt = eTop / 100;
+    //     var curTime = 0;
+    //     while (curTime <= time) {
+    //         window.setTimeout(SVS_B, curTime, eAmt, where);
+    //         curTime += time / 100;
+    //     }
+    // }
     
-    function SVS_B(eAmt, where) {
-        if(where == "center" || where == "")
-            window.scrollBy(0, eAmt / 2);
-        if (where == "top")
-            window.scrollBy(0, eAmt);
-    }
+    // function SVS_B(eAmt, where) {
+    //     if(where == "center" || where == "")
+    //         window.scrollBy(0, eAmt / 2);
+    //     if (where == "top")
+    //         window.scrollBy(0, eAmt);
+    // }
 
-    $(document).on('click', '.menu__link', function () {
-        const myelement = document.querySelector(`${$(this).find('span').attr("data-href")}`)
-        console.log(myelement)
-        SmoothVerticalScrolling(myelement, 400, "top");
-    })
+    // $(document).on('click', '.menu__link', function () {
+    //     const myelement = document.querySelector(`${$(this).find('span').attr("data-href")}`)
+    //     console.log(myelement)
+    //     SmoothVerticalScrolling(myelement, 400, "top");
+    // })
+
+    // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  }
+});
 });
